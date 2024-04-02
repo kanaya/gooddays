12a517c1f62e8c10f574808989870589910ea483
Ichi Kanaya <kanaya@pineapple.cc>
2024-04-02 15:22:28 +0900

    Initial commit.

---
+++ gooddays.py
@@ -0,0 +1,27 @@
+# option-generator.py
+
+import datetime
+import parsedatetime as pdt
+
+start = "April 8, 2024"
+end = "April 26, 2024"
+slots = ["1限目", "2限目", "3限目", "4限目", "5限目"]
+
+cal = pdt.Calendar()
+start_date, _ = cal.parseDT(start)
+end_date, _ = cal.parseDT(end)
+
+def date_range(start, end):
+	current = start
+	while current < end:
+		if current.weekday() < 5:
+			yield current
+		current += datetime.timedelta(days=1)
+
+for d in date_range(start_date, end_date):
+	dd = d.strftime("%m/%d")
+	for s in slots:
+		print("{} {}".format(dd, s))
+
+
+
