231ea98435e5e92130d60898581e55e099b03003
Ichi Kanaya <kanaya@pineapple.cc>
2024-07-24 13:00:29 +0900

    Now takes command-line arguments.

--- gooddays.py
+++ gooddays.py
@@ -2,21 +2,24 @@
 # 開始日から終了日までスロット付きで印字します．「調整さん」にコピペする文字列として便利です．
 # 土日祝日は候補日から外します．
 
+import argparse
 import datetime
 import parsedatetime as pdt
 import jpholiday
 import locale
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--start", type=str, help="Start")
+parser.add_argument("-e", "--end", type=str, help="End")
+args = parser.parse_args()
+
 locale.setlocale(locale.LC_TIME, "ja_JP.UTF-8")
 
-# 開始日（ここを書き換えて下さい）
-start = "April 8, 2024"
-
-# 終了日（ここを書き換えて下さい）
-end = "April 30, 2024"
+start = args.start
+end = args.end
 
 # 各候補日のスロット（ここを書き換えて下さい）
-slots = ["1限目", "2限目", "3限目", "4限目", "5限目"]
+slots = [f"{n}限目" for n in range(1, 6)]
 
 cal = pdt.Calendar()
 start_date, _ = cal.parseDT(start)
