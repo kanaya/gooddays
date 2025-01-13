e28a6056859e123e8c08af281a1d349eacbf79bc
Ichi Kanaya <kanaya@pineapple.cc>
2025-01-13 10:07:55 +0900

    "--sufix" option.

--- gooddays.py
+++ gooddays.py
@@ -12,7 +12,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--start", type=str, help="Start", default="today")
 parser.add_argument("-e", "--end", type=str, help="End", default="tomorrow")
 parser.add_argument("-l", "--slots", type=str, help="Slots", default="1,2,3,4,5")
-parser.add_argument("-f", "--format", type=str, help="Format", default="{} {}")
+parser.add_argument("-x", "--suffix", type=str, help="Suffix", default="時限目")
+parser.add_argument("-f", "--format", type=str, help="Format", default="{} {}{}")
 args = parser.parse_args()
 
 locale.setlocale(locale.LC_TIME, "ja_JP.UTF-8")
@@ -20,6 +21,7 @@ locale.setlocale(locale.LC_TIME, "ja_JP.UTF-8")
 start = args.start
 end = args.end
 slots = args.slots
+suffix = args.suffix
 format_str = args.format
 
 # 各候補日のスロット（ここを書き換えて下さい）
@@ -39,4 +41,4 @@ def date_range(start, end):
 for d in date_range(start_date, end_date):
 	dd = d.strftime("%m/%d (%a)")
 	for s in slot_list:
-		print(format_str.format(dd, s))
+		print(format_str.format(dd, s, suffix))
