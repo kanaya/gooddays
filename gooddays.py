695746ef1b7dc3534e7e341771dcc8ff6c57306e
Ichi Kanaya <kanaya@pineapple.cc>
2025-06-07 17:13:21 +0900

    Added -n option.

--- gooddays.py
+++ gooddays.py
@@ -14,6 +14,7 @@ parser.add_argument("-e", "--end", type=str, help="End", default="tomorrow")
 parser.add_argument("-l", "--slots", type=str, help="Slots", default="1,2,3,4,5")
 parser.add_argument("-x", "--suffix", type=str, help="Suffix", default="時限目")
 parser.add_argument("-f", "--format", type=str, help="Format", default="{} {}{}")
+parser.add_argument("-n", "--noholiday", action='store_true', help="No Holiday")
 args = parser.parse_args()
 
 locale.setlocale(locale.LC_TIME, "ja_JP.UTF-8")
@@ -23,8 +24,9 @@ end = args.end
 slots = args.slots
 suffix = args.suffix
 format_str = args.format
+noholiday = bool(args.noholiday)
 
-# 各候補日のスロット（ここを書き換えて下さい）
+# 各候補日のスロット
 slot_list = slots.split(",")
 
 cal = pdt.Calendar()
@@ -33,10 +35,15 @@ end_date, _ = cal.parseDT(end)
 
 def date_range(start, end):
 	current = start
-	while current <= end:
-		if current.weekday() < 5 and not jpholiday.is_holiday(current):
+	if noholiday:
+		while current <= end:
 			yield current
-		current += datetime.timedelta(days=1)
+			current += datetime.timedelta(days=1)
+	else:
+		while current <= end:
+			if current.weekday() < 5 and not jpholiday.is_holiday(current):
+				yield current
+			current += datetime.timedelta(days=1)
 
 for d in date_range(start_date, end_date):
 	dd = d.strftime("%m/%d (%a)")
